5a1d16d45a0220f443b327cfbc060ed92cb468af
AZELVIN <118862765+AZELVIN@users.noreply.github.com>
2022-11-24 22:17:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$('#card1').on('click', function () {
+  $('#popup1').removeClass('member-popup-hidden');
+});
+
+$('#card2').on('click', function () {
+  $('#popup2').removeClass('member-popup-hidden');
+});
+
+$('#card3').on('click', function () {
+  $('#popup3').removeClass('member-popup-hidden');
+});
+
+$('.member-popup').on('click', function () {
+  $('.member-popup').addClass('member-popup-hidden');
+});
